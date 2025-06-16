4c1ff71e9758b3c2c78976fa52b9420a6fc9f669
pmilan63 <pmilan632004@gmail.com>
2025-06-16 20:10:17 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from typing import List
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''This function will return a list of requirements from the given file path.'''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+            
+    return requirements
+
+setup(
+    name='my_package',
+    version='0.0.1',
+    author='Milan',
+    author_email='pmilan632004@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)

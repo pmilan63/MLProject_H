8b4d43bad952035e2335897721e9108215d8440a
pmilan63 <pmilan632004@gmail.com>
2025-06-19 10:00:47 +0530

    Model Trainer

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformation
 from src.mlproject.components.data_transformation import DataTransformationConfig
 
+from src.mlproject.components.model_trainer import ModelTrainer
+from src.mlproject.components.model_trainer import ModelTrainerConfig
+
 
 if __name__ == "__main__":
     logging.info("Starting the application...")
@@ -19,7 +22,10 @@ if __name__ == "__main__":
 
         # data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
     except Exception as e:
         logging.info("divided by zero error.")
